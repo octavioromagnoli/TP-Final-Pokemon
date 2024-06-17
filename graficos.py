10ede0892bcd07f112f56c60f272a7504b59d60b
fermin6 <min.salmain@gmail.com>
2024-06-17 17:39:05 -0300

    Grafico aotitud x epoca, correccion del primero

--- graficos.py
+++ graficos.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-
+""""
 def diversidad_x_epoca():
     with open('epochs_prueba.csv', 'r') as file:
         epochs = file.readlines()
@@ -13,6 +13,10 @@ def diversidad_x_epoca():
         print(elementos)
         epocas.append(int(elementos[0])) 
         diversidad.append(sum(1 for elem in elementos[1:] if not elem.replace('.', '', 1).isdigit()))
+        Así deberia ser si no se mostrará la diversidad de cada pokemon en el archivo, 
+        lo cual si tiene que devolever solo que no se especifica en el tp.
+        
+        diversidad.append(int(elementos[1]))
         i += 1
 
     print(epocas)
@@ -24,3 +28,26 @@ def diversidad_x_epoca():
     plt.show()
 
 diversidad_x_epoca()
+"""
+def aptitud_x_epoca():
+    with open('best_teams.csv', 'r') as file:
+        best_teams = file.readlines()
+    epocas = []
+    aptitud = []
+    i = 1
+    for linea in best_teams:
+        elementos = [elem.strip() for elem in linea.split(',')]
+        print(elementos)
+        epocas.append(int(elementos[0])) 
+        aptitud.append(float(elementos[1]))
+        i += 1
+
+    print(epocas)
+    print(aptitud)
+    plt.plot(epocas, aptitud, color='m')
+    plt.xlabel('Epocas')
+    plt.ylabel('Aptitud')
+    plt.title('Aptitud por epoca')
+    plt.show()
+
+aptitud_x_epoca()
