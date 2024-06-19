57df8fe443bd35d9b216400b01ac735bf63c59d9
fermin6 <min.salmain@gmail.com>
2024-06-19 16:40:05 -0300

    Terminados los 4 graficos, funcionan perfecto, se les puede mejorar algunos detalles

--- graficos.py
+++ graficos.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-""""
+import numpy as np
+
 def diversidad_x_epoca():
     with open('epochs_prueba.csv', 'r') as file:
         epochs = file.readlines()
@@ -12,10 +13,11 @@ def diversidad_x_epoca():
         elementos = [elem.strip() for elem in linea.split(',')]
         print(elementos)
         epocas.append(int(elementos[0])) 
+        """
         diversidad.append(sum(1 for elem in elementos[1:] if not elem.replace('.', '', 1).isdigit()))
         Así deberia ser si no se mostrará la diversidad de cada pokemon en el archivo, 
         lo cual si tiene que devolever solo que no se especifica en el tp.
-        
+        """
         diversidad.append(int(elementos[1]))
         i += 1
 
@@ -28,7 +30,7 @@ def diversidad_x_epoca():
     plt.show()
 
 diversidad_x_epoca()
-"""
+
 def aptitud_x_epoca():
     with open('best_teams.csv', 'r') as file:
         best_teams = file.readlines()
@@ -51,3 +53,70 @@ def aptitud_x_epoca():
     plt.show()
 
 aptitud_x_epoca()
+
+def pokemones_ultima_epoca():
+    with open('epochs_prueba.csv', 'r') as file:
+        epochs = file.readlines()
+        ultima_epoca = epochs[-1].strip().split(',')
+        cantidad_cada_pokemon = []
+        pokemones = []
+        for i in range(2, len(ultima_epoca), 2):
+            cantidad_cada_pokemon.append(int(ultima_epoca[i+1]))
+            pokemones.append(ultima_epoca[i])
+        
+    print(pokemones)
+    print(cantidad_cada_pokemon)
+    plt.figure(figsize=(10, 6))  # Tamaño de la figura
+    plt.bar(pokemones, cantidad_cada_pokemon, color='m')
+    plt.xlabel('Pokemones')
+    plt.ylabel('Cantidad')
+    plt.title('Distribucion de pokemones en los equipos, en la Última Época')
+    
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    plt.show()
+
+pokemones_ultima_epoca()
+
+def estadisticas_mejor_equipo():
+    with open('best_teams.csv', 'r') as file:
+        best_teams = file.readlines()
+        ultima_epoca = best_teams[-1].strip().split(',')[0]
+        i = 0
+        while best_teams[i].strip().split(',')[0] != ultima_epoca:
+            i += 1
+        
+        equipo = best_teams[i].strip().split(',')[4:10]  
+        pokemones = equipo
+            
+        print("Pokémon del mejor equipo:")
+        print(pokemones)
+    estadisticas_pokemones = []
+    pokemon_df = pd.read_csv('pokemons.csv')
+    stats_columns = pokemon_df[['name', 'hp', 'attack', 'defense', 'sp_attack', 'sp_defense', 'speed']]
+    for pokemon_name in pokemones:
+        pokemon_stats = stats_columns[stats_columns['name'] == pokemon_name].values.tolist()
+        estadisticas_pokemones.append(pokemon_stats[0][1:])
+    print(estadisticas_pokemones)
+    
+    # Radar chart
+
+    angles = np.linspace(0, 2*np.pi, 6, endpoint=False).tolist()
+    angles += angles[:1]
+
+    fig, ax = plt.subplots(figsize=(8, 8), subplot_kw=dict(polar=True))
+    for i, (pokemon_name, stats) in enumerate(zip(pokemones, estadisticas_pokemones)):
+        stats += stats[:1]
+        ax.plot(angles, stats, label=pokemon_name)
+        ax.fill(angles, stats, alpha=0.25)
+  
+    ax.set_theta_offset(np.pi / 2)
+    ax.set_theta_direction(-1)
+    ax.set_xticks(angles[:-1])
+    ax.set_xticklabels(['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed'])
+
+    plt.title('Radar Chart de Estadísticas de Pokémon')
+    plt.legend(loc='upper right', bbox_to_anchor=(1.3, 1.1))
+    plt.show()
+    
+estadisticas_mejor_equipo()
